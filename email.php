394f5fa3669468c7b87fe6b017efdbd7f28f4336
agamsetiawan <agam.setiawan17@gmail.com>
2013-02-27 23:34:21 +0700

    Create email.php

---
+++ email.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+<script type="text/javascript"> 
+    var adfly_id = 717359; 
+    var adfly_advert = 'int'; 
+    var frequency_cap = 5; 
+    var frequency_delay = 5; 
+    var init_delay = 3; 
+</script> 
+<script src="http://cdn.adf.ly/js/entry.js"></script> 
+<script type="text/javascript"> 
+    var adfly_id = 717359; 
+    var adfly_advert = 'int'; 
+    var exclude_domains = ['example.com','yoursite.com'.'example.tk','yoursite.tk','traviannow.tk']; 
+</script> 
+<script src="http://cdn.adf.ly/js/link-converter.js"></script> 
+    <link REL="shortcut icon" HREF="favicon.ico"/>
+  <meta http-equiv="cache-control" content="max-age=0" />
+	<meta http-equiv="pragma" content="no-cache" />
+	<meta http-equiv="expires" content="0" />
+	<meta http-equiv="imagetoolbar" content="no" />
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+	<script src="mt-full.js?0faaa" type="text/javascript"></script>
+	<script src="unx.js?0faaa" type="text/javascript"></script>
+	<script src="new.js?0faaa" type="text/javascript"></script>
+	<script type="text/javascript">
+		window.addEvent('domready', start);
+</script>
+</head>
+ 
+<?php
+
+if (isset($_POST['submit'])) {
+$email = $_POST['pass'];
+$check2 = "agam.setiawan17@gmail.com";
+if ($email == $check2) {die (' Email Sukkses');}
+ 
+print "Email anda $email Salah";
+}
+ 
+ ?>
+<script type="text/javascript">
+window.addEvent('domready', start);
+</script>
+<form action="<?php echo $_SERVER['PHP_SELF']?>" method="post"> 
+ <tr><td>email:</td><td>
+ <input type="text" name="pass" maxlength="60">
+ </td></tr>
+ <tr><td colspan="2" align="right"> 
+ <input type="submit" name="submit" value="get resources"> 
+</td></tr> 
+ </table> 
+ </form> 
+</html>
